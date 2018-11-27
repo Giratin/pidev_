a8e554e5f9af36039dec20790589f78e31818104
yassine sta <stayassine3@gmail.com>
2018-11-28 00:16:50 +0100

    Reservation
    added reservation in repository

--- src/ExcursionBundle/Controller/RandonneController.php
+++ src/ExcursionBundle/Controller/RandonneController.php
@@ -4,8 +4,10 @@ namespace ExcursionBundle\Controller;
 
 use ExcursionBundle\Entity\Imagesrando;
 use ExcursionBundle\Entity\Randonne;
+use ExcursionBundle\Entity\Reservationrandonne;
 use ExcursionBundle\Form\RandonneType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 
 
@@ -36,37 +38,119 @@ class RandonneController extends Controller
 
     public function newAction(Request $request)
     {
-       $randonne = new Randonne();
-        $form = $this->createForm(RandonneType::class, $randonne);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
+        $user = $this->getUser();
+        if($user == null || $user->getRoles()[0] == 'ROLE_USER')
+        {
             $em = $this->getDoctrine()->getManager();
-            $em->persist($randonne);
-            $em->flush();
+
+            $listRando = $this->getDoctrine()->getRepository(Randonne::class)->myFindByNewDate();
+            $count = $this->getDoctrine()->getRepository(Randonne::class)->countNewExcursion();
+
+            //$listRando = $this->getDoctrine()->getRepository(Randonne::class)->findAll();
 
 
-            return $this->redirectToRoute('randonne_show', array('idrando' => $randonne->getIdrando()));
+            $randonne = $this->get('knp_paginator')->paginate(
+                $listRando,
+                $request->query->get('page',1),6
+            );
+
+            return $this->redirectToRoute('randonne_index', array(
+                'randonnes' => $randonne,
+                'count' => $count
+                ));
         }
 
-        return $this->render('@Excursion/randonne/new.html.twig', array(
-            'randonne' => $randonne,
-            'form' => $form->createView(),
-        ));
+        else if($user->getRoles()[0] == 'ROLE_ADMIN')
+        {
+            $randonne = new Randonne();
+            $form = $this->createForm(RandonneType::class, $randonne);
+            $form->handleRequest($request);
+            $new = $this->getDoctrine()->getRepository(Randonne::class)->countNewExcursion();
+            $old = $this->getDoctrine()->getRepository(Randonne::class)->countOldExcursion();
+            $sum = $this->getDoctrine()->getRepository(Randonne::class)->countAllExcursion();
+            if ($form->isSubmitted() && $form->isValid()) {
+                /**
+                 * @var UploadedFile $file1
+                 */
+                $file1 = $randonne->getImgurl1();
+                //encodage des images pour assurer l'unicité du nom de l'image sauvgardée
+                $filename1 = md5(uniqid()).'.'.$file1->guessExtension();
 
+                //transfert de l'image au dossier destination
+                $file1->move($this->getParameter('image_directory'),$filename1);
+                $randonne->setImgurl1($filename1);
+
+                /**
+                 * @var UploadedFile $file2
+                 */
+                $file2 = $randonne->getImgurl2();
+                //encodage des images pour assurer l'unicité du nom de l'image sauvgardée
+                $filename2 = md5(uniqid()).'.'.$file2->guessExtension();
+
+                //transfert de l'image au dossier destination
+                $file2->move($this->getParameter('image_directory'),$filename2);
+                $randonne->setImgurl1($filename2);
+
+                /**
+                 * @var UploadedFile $file3
+                 */
+                $file3 = $randonne->getImgurl3();
+                //encodage des images pour assurer l'unicité du nom de l'image sauvgardée
+                $filename3 = md5(uniqid()).'.'.$file3->guessExtension();
+
+                //transfert de l'image au dossier destination
+                $file3->move($this->getParameter('image_directory'),$filename3);
+                $randonne->setImgurl3($filename3);
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($randonne);
+                $em->flush();
+
+                return $this->redirectToRoute('randonne_show', array('idrando' => $randonne->getIdrando()));
+            }
+
+            return $this->render('@Excursion/randonne/new.html.twig', array(
+                'randonne' => $randonne,
+                'form' => $form->createView(),
+                'newcount' => $new,
+                'oldcount' => $old,
+                'sumcount' => $sum,
+            ));
+        }
+        else
+            return $this->render('@Excursion/randonne/index.html.twig');
 
     }
 
     public function showAction(Randonne $randonne)
     {
         $deleteForm = $this->createDeleteForm($randonne);
+        $user = $this->getUser();
+        $role = $user->getRoles()[0];
+        $error = null;
+        if($role == 'ROLE_ADMIN')
+            $can = 'yes';
+        else
+            $can ='no';
+        $idUser = $user->getId();
+        $idRando = $randonne->getIdrando();
+        $verify = $this->getDoctrine()
+            ->getRepository(Reservationrandonne::class)
+            ->myfindMe($idUser, $idRando);
+        if($verify == null)
+        {
+            $error = null;
+        }
+        else
+            $error =1;
 
         return $this->render('@Excursion/randonne/show.html.twig', array(
             'randonne' => $randonne,
-            'delete_form' => $deleteForm->createView()
+            'delete_form' => $deleteForm->createView(),
+            'role' => $can,
+            'error' => $error,
         ));
     }
-
 
     public function editAction(Request $request, Randonne $randonne)
     {
@@ -75,6 +159,40 @@ class RandonneController extends Controller
         $editForm->handleRequest($request);
 
         if ($editForm->isSubmitted() && $editForm->isValid()) {
+            /**
+             * @var UploadedFile $file1
+             */
+            $file1 = $randonne->getImgurl1();
+            //encodage des images pour assurer l'unicité du nom de l'image sauvgardée
+            $filename1 = md5(uniqid()).'.'.$file1->guessExtension();
+
+            //transfert de l'image au dossier destination
+            $file1->move($this->getParameter('image_directory').$filename1);
+            $randonne->setImgurl1($filename1);
+
+            /**
+             * @var UploadedFile $file2
+             */
+            $file2 = $randonne->getImgurl2();
+            //encodage des images pour assurer l'unicité du nom de l'image sauvgardée
+            $filename2 = md5(uniqid()).'.'.$file2->guessExtension();
+
+            //transfert de l'image au dossier destination
+            $file2->move($this->getParameter('image_directory').$filename2);
+            $randonne->setImgurl1($filename2);
+
+
+            /**
+             * @var UploadedFile $file3
+             */
+            $file3 = $randonne->getImgurl3();
+            //encodage des images pour assurer l'unicité du nom de l'image sauvgardée
+            $filename3 = md5(uniqid()).'.'.$file3->guessExtension();
+
+            //transfert de l'image au dossier destination
+            $file3->move($this->getParameter('image_directory'),$filename3);
+            $randonne->setImgurl3($filename3);
+
             $this->getDoctrine()->getManager()->flush();
 
             return $this->redirectToRoute('randonne_edit', array('idrando' => $randonne->getIdrando()));
@@ -82,7 +200,7 @@ class RandonneController extends Controller
 
         return $this->render('@Excursion/randonne/edit.html.twig', array(
             'randonne' => $randonne,
-            'edit_form' => $editForm->createView(),
+            'form' => $editForm->createView(),
             'delete_form' => $deleteForm->createView(),
         ));
     }
@@ -105,7 +223,6 @@ class RandonneController extends Controller
         return $this->redirectToRoute('randonne_index');
     }
 
-
     private function createDeleteForm(Randonne $randonne)
     {
         return $this->createFormBuilder()
@@ -114,8 +231,6 @@ class RandonneController extends Controller
             ->getForm()
         ;
     }
-
-
 
     public function oldAction(Request $request)
     {
@@ -132,6 +247,80 @@ class RandonneController extends Controller
             'randonnes' => $randonne,
             'num' => $count,
         ));
+
+    }
+
+    public function newadminAction(Request $request)
+    {
+        $user = $this->getUser();
+
+        $listRando = $this->getDoctrine()->getRepository(Randonne::class)->myFindByNewDate();
+        $count = $this->getDoctrine()->getRepository(Randonne::class)->countNewExcursion();
+
+        $randonne = $this->get('knp_paginator')->paginate(
+            $listRando,
+            $request->query->get('page',1),10
+        );
+
+        if($user == null)
+        {
+            return $this->redirectToRoute('randonne_index', array(
+                'randonnes' => $randonne,
+                'count' => $count
+            ));
+        }
+        else if($user->getRoles()[0] == 'ROLE_ADMIN')
+        {
+            //$randonne = new Randonne();
+            $new = $this->getDoctrine()->getRepository(Randonne::class)->countNewExcursion();
+            $old = $this->getDoctrine()->getRepository(Randonne::class)->countOldExcursion();
+            $sum = $this->getDoctrine()->getRepository(Randonne::class)->countAllExcursion();
+
+            return $this->render('@Excursion/randonne/newAdmin.html.twig', array(
+                'randonne' => $randonne,
+                'newcount' => $new,
+                'oldcount' => $old,
+                'sumcount' => $sum,
+            ));
+        }
+        else
+            return $this->render('@Excursion/randonne/index.html.twig');
+    }
+
+    public function reserveAction(Request $request, Randonne $randonne)
+    {
+        $user = $this->getUser();
+        if($user != null)
+        {
+            if($user->getRoles()[0] == 'ROLE_USER')
+            {
+                $idUser = $user->getId();
+                $idRando = $randonne->getIdrando();
+                $verify = $this->getDoctrine()
+                    ->getRepository(Reservationrandonne::class)
+                    ->myfindMe($idUser, $idRando);
+
+                if($verify == null)
+                {
+                    $reservation = new Reservationrandonne();
+                    $reservation->setIdClient($idUser);
+                    $reservation->setIdRandonne($idRando);
+                    //die($idUser. ' ra: ' . $idRando);
+                    $em = $this->getDoctrine()->getManager();
+                    $em->persist($reservation);
+                    $em->flush();
+                    return $this->redirectToRoute('randonne_index');
+                }
+
+                else
+                {
+                    $error = 1;
+                    return $this->redirectToRoute('randonne_index', array(
+                        'error' => $error
+                    ));
+                }
+            }
+        }
 
     }
 }
---
+++ src/ExcursionBundle/Repository/ReservationrandonneRepository.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: STA
+ * Date: 27/11/2018
+ * Time: 23:44
+ */
+
+namespace ExcursionBundle\Repository;
+
+
+class ReservationrandonneRepository extends \Doctrine\ORM\EntityRepository
+{
+    public function myfindMe($idUser, $idRando)
+    {
+        $dql = $this
+            ->getEntityManager()
+            ->createQuery('SELECT R FROM ExcursionBundle:Reservationrandonne R WHERE R.idRandonne = :idr AND R.idClient = :idc');
+        $dql->setParameter(':idr',$idRando)
+            ->setParameter(':idc', $idUser);
+        return $dql->getResult();
+    }
+}
